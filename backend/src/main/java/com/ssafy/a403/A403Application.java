11c3107b1165339d4564969604f86672d68d1a13
Jiwon <mydowson@gmail.com>
2023-07-17 15:52:17 +0900

    Initial commit backend

---
+++ backend/src/main/java/com/ssafy/a403/A403Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.a403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A403Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A403Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/a403/A403ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.a403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A403ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
