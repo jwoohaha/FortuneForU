22bd6f2b9e5a62e23429970b7570fb048a9092ea
soyun <soun997@gmail.com>
2023-07-28 11:56:40 +0900

    feat: 소셜 로그인을 위한 AttributeMapper Factory 클래스 추가

---
+++ backend/src/main/java/com/ssafy/a403/config/security/oauth/mapper/AttributeMapper.java
@@ -0,0 +1,10 @@
+package com.ssafy.a403.config.security.oauth.mapper;
+
+import com.ssafy.a403.config.security.oauth.OAuth2Request;
+
+import java.util.Map;
+
+public interface AttributeMapper {
+	OAuth2Request mapToDto(Map<String, Object> attributes);
+
+}
