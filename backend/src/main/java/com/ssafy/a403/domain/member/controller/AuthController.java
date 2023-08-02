ec4664d40411ca235e8cce7cad8b8fe34cd3d020
soyun <soun997@gmail.com>
2023-08-02 17:57:19 +0900

    feat: 로그 추가

--- backend/src/main/java/com/ssafy/a403/domain/member/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/a403/domain/member/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.ssafy.a403.domain.member.dto.AuthRequest;
 import com.ssafy.a403.domain.member.dto.AuthResponse;
 import com.ssafy.a403.global.config.security.jwt.JwtSetupService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,12 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthController {
 
     private final JwtSetupService jwtSetupService;
 
     @PostMapping
     public ResponseEntity<AuthResponse> authToken(@RequestBody @Validated AuthRequest authRequest) {
+        log.trace("Auth Token: {}", authRequest.getAuthToken());
+
         return ResponseEntity.ok(
                 AuthResponse.of(
                         jwtSetupService.createJwtTokenByAuthToken(authRequest.getAuthToken())));
