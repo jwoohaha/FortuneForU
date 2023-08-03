d6d46b53250b1a67900fd41bba4dd659877a56e5
soyun <soun997@gmail.com>
2023-08-03 15:49:22 +0900

    feat: spring 서버와의 통신을 위한 base api 생성
    header에 token을 넣어서 함께 보냄

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,8 +1,17 @@
 import axios from 'axios'
+import { useTokenStore } from "@/stores/token"
+
+const store = useTokenStore();
 
 function apiInstance() {
+    
     const instance = axios.create({
-        baseURL: 'http://localhost:5000'
+        baseURL: 'http://localhost:5000',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            'Authorization': store.getAccessToken,
+            'Refresh': store.getRefreshToken
+        }
     });
     return instance;
 }
