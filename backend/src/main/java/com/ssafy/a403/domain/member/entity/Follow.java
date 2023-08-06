67911566b923528ee7159588228604153b479c34
sypark <soun997@gmail.com>
2023-08-06 12:14:03 +0900

    feat: JpaAuditing 코드 추가 - 생성시간, 수정시간 필드 자동 추가
    insert 시 부가적으로 발생하는 select 쿼리를 방지하기 위함

--- backend/src/main/java/com/ssafy/a403/domain/member/entity/Follow.java
+++ backend/src/main/java/com/ssafy/a403/domain/member/entity/Follow.java
@@ -1,8 +1,11 @@
 package com.ssafy.a403.domain.member.entity;
 
+import com.ssafy.a403.global.audit.BaseTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -10,12 +13,17 @@ import java.io.Serializable;
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Follow {
+public class Follow extends BaseTime implements Persistable<FollowId> {
 
     @EmbeddedId
     private FollowId id;
 
     public Follow(Member follower, Member followee) {
         this.id = new FollowId(follower, followee);
+    }
+
+    @Override
+    public boolean isNew() {
+        return getCreated() == null;
     }
 }
