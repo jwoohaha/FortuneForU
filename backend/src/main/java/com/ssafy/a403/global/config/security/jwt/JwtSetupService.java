a71968e5c661907661627e0456eca58f66fa1352
soyun <soun997@gmail.com>
2023-08-10 14:23:18 +0900

    feat: setCookie() 메서드에 httpOnly, secure 옵션 추가

--- backend/src/main/java/com/ssafy/a403/global/config/security/jwt/JwtSetupService.java
+++ backend/src/main/java/com/ssafy/a403/global/config/security/jwt/JwtSetupService.java
@@ -26,18 +26,18 @@ public class JwtSetupService {
     @Value("${jwt.refresh-header}")
     private String refreshTokenHeaderTag;
 
-    public String addJwtTokensToCookie(HttpServletResponse response, LoginUser loginUser) {
-        JwtToken jwtToken = jwtProvider.createJwtToken(loginUser);
-
-        ResponseCookie accessTokenCookie = setCookie(accessTokenHeaderTag, jwtToken.getAccessToken());
-        ResponseCookie refreshTokenCookie = setCookie(refreshTokenHeaderTag, jwtToken.getRefreshToken());
-        log.info("accessToken = " + accessTokenCookie);
-        log.info("refreshToken = " + refreshTokenCookie);
-        response.addHeader("Set-Cookie", accessTokenCookie.toString());
-        response.addHeader("Set-Cookie", refreshTokenCookie.toString());
-        
-        return jwtToken.getAccessToken();
-    }
+//    public String addJwtTokensToCookie(HttpServletResponse response, LoginUser loginUser) {
+//        JwtToken jwtToken = jwtProvider.createJwtToken(loginUser);
+//
+//        ResponseCookie accessTokenCookie = setCookie(accessTokenHeaderTag, jwtToken.getAccessToken());
+//        ResponseCookie refreshTokenCookie = setCookie(refreshTokenHeaderTag, jwtToken.getRefreshToken());
+//        log.info("accessToken = " + accessTokenCookie);
+//        log.info("refreshToken = " + refreshTokenCookie);
+//        response.addHeader("Set-Cookie", accessTokenCookie.toString());
+//        response.addHeader("Set-Cookie", refreshTokenCookie.toString());
+//
+//        return jwtToken.getAccessToken();
+//    }
     
     public String createAuthToken(LoginUser loginUser) {
     	return jwtProvider.createAuthToken(loginUser);
@@ -47,9 +47,11 @@ public class JwtSetupService {
     	return jwtProvider.createJwtTokenByAuthToken(authToken);
     }
 
-    private ResponseCookie setCookie(String key, String value) {
+    public ResponseCookie setCookie(String key, String value) {
         return ResponseCookie.from(key, value)
                 .path("/")
+                .httpOnly(true)
+                .secure(true)
                 .domain(clientHost)
                 .build();
     }
