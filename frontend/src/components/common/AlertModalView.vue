02c069d4b1bbd7c599de809dff2b39103d8d4630
KangInyeong <kiyoog02@gmail.com>
2023-08-10 17:57:23 +0900

    feat: make alert modal

---
+++ frontend/src/components/common/AlertModalView.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="modal">
+      <div class="modal-overlay" @click="$emit('close-modal')"></div>
+      <modal-content modalWidth="3" modalHeight="1.2">
+        <div class="modal-box">
+            안녕
+        </div>
+      </modal-content>
+    </div>
+  </template>
+  
+  <script>
+  import { ModalContent } from "@/components/styled-components/StyledModal.js";
+
+  
+  export default {
+    components: {
+      ModalContent,
+      
+    },
+  };
+  </script>
+  
+  <style lang="scss">
+  .modal,
+  .modal-overlay {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+  }
+  
+  .modal-overlay {
+    opacity: 0.5;
+    background-color: #000000;
+  }
+
+  .modal-box{
+    height: 100%;
+    width: 100%;
+    background: #ffffff;
+  }
+  </style>
+  
