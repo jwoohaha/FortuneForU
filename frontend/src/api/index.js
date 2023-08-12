13a878d6f5750f7eded64e54822483d235fa7a16
sypark <soun997@gmail.com>
2023-08-12 23:44:55 +0900

    feat: apiInstance() 메서드에 axios interceptor를 추가하여 header 생성

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -7,12 +7,22 @@ function apiInstance() {
 
     const instance = axios.create({
         baseURL: 'https://i9a403.p.ssafy.io/api',
-        //baseURL: 'http://localhost:5000/api',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-            'Authorization': store.getAccessToken
-        }
+        // baseURL: 'http://localhost:5000/api',
     });
+
+    instance.interceptors.request.use(
+        (config) => {
+            config.headers['Content-Type'] = 'application/json;charset=utf-8';
+            config.headers['Authorization'] = store.getAccessToken;
+            
+            return config;
+        },
+        (error) => {
+            console.log("interceptor error: " + error);
+            return Promise.reject(error);
+        }
+    )
+
     return instance;
 }
 
