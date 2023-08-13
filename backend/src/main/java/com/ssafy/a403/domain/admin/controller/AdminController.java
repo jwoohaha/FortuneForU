a39fc43192cbc14bd40db3967ab7d472364d694e
soyun <soun997@gmail.com>
2023-08-13 18:56:55 +0900

    feat: Counselor Form 등록 및 목록 조회 Request Mapping 메서드 구현

--- backend/src/main/java/com/ssafy/a403/domain/admin/controller/AdminController.java
+++ backend/src/main/java/com/ssafy/a403/domain/admin/controller/AdminController.java
@@ -1,40 +1,36 @@
 package com.ssafy.a403.domain.admin.controller;
 
-import com.ssafy.a403.domain.member.dto.FormResponse;
 import com.ssafy.a403.domain.admin.service.AdminService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import com.ssafy.a403.domain.counselorform.dto.CounselorFormResponse;
+import com.ssafy.a403.domain.counselorform.entity.CounselorForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
-@RequestMapping("/admin")
+@RequestMapping("/api/admin")
 @RestController
 @RequiredArgsConstructor
 public class AdminController {
 
-    private AdminService adminService;
+    private final AdminService adminService;
 
-    // 상담사신청정보 불러오기
-    @ApiOperation(value = "사용자 정보", notes = "신청된 사용자 정보를 반환합니다")
-    @ApiResponses({@ApiResponse(code = 200, message = "성공"),
-            @ApiResponse(code = 401, message = "인증실패"),@ApiResponse(code = 404, message = "페이지 없음"),
-            @ApiResponse(code = 500, message = "서버 오류")})
-    @GetMapping("/forms")
-    public ResponseEntity<?> getAllForms(){
-
-        log.info("상담사신청정보 시작-----------------------------");
-        List<FormResponse> formResponseList = adminService.getAllForms();
-        log.info("상담사신청정보 끝-------------------------------");
-
-        return new ResponseEntity<>(formResponseList, HttpStatus.OK);
+    @GetMapping("/counselor-forms")
+    public ResponseEntity<List<CounselorFormResponse>> counselorForms(@RequestParam String filter, Pageable pageable){
+        log.trace(filter);
+        Page<CounselorForm> forms = adminService.counselorFormList(filter, pageable);
+        return new ResponseEntity<>(forms.stream()
+                .map(CounselorFormResponse::from)
+                .collect(Collectors.toList()), HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/ssafy/a403/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/a403/domain/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.ssafy.a403.domain.member.controller;
 
+import com.ssafy.a403.domain.counselorform.dto.CounselorFormRequest;
 import com.ssafy.a403.domain.member.dto.MemberDetailsResponse;
 import com.ssafy.a403.domain.member.dto.MemberInfoResponse;
 import com.ssafy.a403.domain.member.entity.Member;
@@ -22,16 +23,6 @@ import javax.servlet.http.HttpServletRequest;
 public class MemberController {
 
     private final MemberService memberService;
-    private final FollowService followService;
-
-    @GetMapping("/test")
-    public ResponseEntity testMethod(HttpServletRequest request) {
-        log.trace("====== TEST ======");
-        log.trace("access-token: {}", request.getHeader("Authorization"));
-        log.trace("refresh-token: {}", request.getHeader("Refresh"));
-
-        return new ResponseEntity(HttpStatus.OK);
-    }
 
     // Member 정보 조회
     @GetMapping("/info")
@@ -63,14 +54,23 @@ public class MemberController {
     @PutMapping("follow/{followeeId}")
     public HttpStatus follow(@PathVariable Long followeeId, @AuthenticationPrincipal LoginUser loginUser) {
 
-        followService.follow(loginUser.getMember(), memberService.findById(followeeId));
-        return HttpStatus.OK;
+        memberService.follow(loginUser.getMember(), memberService.findById(followeeId));
+        return HttpStatus.NO_CONTENT;
     }
 
     @DeleteMapping("unfollow/{followeeId}")
     public HttpStatus unfollow(@PathVariable Long followeeId, @AuthenticationPrincipal LoginUser loginUser) {
 
-        followService.unfollow(loginUser.getMember(), memberService.findById(followeeId));
-        return HttpStatus.OK;
+        memberService.unfollow(loginUser.getMember(), memberService.findById(followeeId));
+        return HttpStatus.NO_CONTENT;
+    }
+
+    @PostMapping("/submit")
+    public HttpStatus submitCounselorForm(
+            @RequestBody CounselorFormRequest counselorFormRequest,
+            @AuthenticationPrincipal LoginUser loginUser){
+
+        memberService.submitCounselorForm(counselorFormRequest, loginUser);
+        return HttpStatus.NO_CONTENT;
     }
 }
