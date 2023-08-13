65bbf58d1195f1b76e27f4fec1926ecdc7bf6312
soyun <soun997@gmail.com>
2023-08-13 18:56:02 +0900

    feat: AdminService - CounselorFormService의 Form 등록 메서드 호출

--- backend/src/main/java/com/ssafy/a403/domain/member/service/MemberService.java
+++ backend/src/main/java/com/ssafy/a403/domain/member/service/MemberService.java
@@ -1,10 +1,14 @@
 package com.ssafy.a403.domain.member.service;
 
+import com.ssafy.a403.domain.counselorform.dto.CounselorFormRequest;
+import com.ssafy.a403.domain.counselorform.service.CounselorFormService;
 import com.ssafy.a403.domain.member.dto.MemberDetailsResponse;
 import com.ssafy.a403.domain.member.dto.MemberInfoResponse;
+import com.ssafy.a403.domain.member.entity.Follow;
 import com.ssafy.a403.domain.member.entity.FollowId;
 import com.ssafy.a403.domain.member.entity.Member;
 import com.ssafy.a403.domain.member.repository.MemberRepository;
+import com.ssafy.a403.global.config.security.LoginUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +23,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberService {
 
+    private final CounselorFormService counselorFormService;
     private final FollowService followService;
     private final MemberRepository memberRepository;
 
@@ -34,6 +39,24 @@ public class MemberService {
 
         List<Member> followers = followService.followerList(member);
         return MemberDetailsResponse.of(member, followers);
+    }
+
+    @Transactional
+    public void follow(Member follower, Member followee) {
+
+        followService.follow(follower, followee);
+    }
+
+    @Transactional
+    public void unfollow(Member follower, Member followee) {
+
+        followService.unfollow(follower, followee);
+    }
+
+    @Transactional
+    public void submitCounselorForm(CounselorFormRequest counselorFormRequest, LoginUser loginUser) {
+
+        counselorFormService.submitForm(counselorFormRequest, loginUser);
     }
 
     public Page<Member> findPaging(Pageable pageable) {
