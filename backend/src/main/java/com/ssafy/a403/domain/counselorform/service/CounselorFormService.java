a283e6189031b160b6f037630c000684d71a311b
soyun <soun997@gmail.com>
2023-08-13 18:55:22 +0900

    feat: CounselorForm 등록 및 목록 조회를 관리하는 Service 구현

--- backend/src/main/java/com/ssafy/a403/domain/counselorform/service/CounselorFormService.java
+++ backend/src/main/java/com/ssafy/a403/domain/counselorform/service/CounselorFormService.java
@@ -1,2 +1,29 @@
-package com.ssafy.a403.domain.counselorform.service;public class CounselorFormService {
+package com.ssafy.a403.domain.counselorform.service;
+
+import com.ssafy.a403.domain.counselorform.dto.CounselorFormRequest;
+import com.ssafy.a403.domain.counselorform.entity.CounselorForm;
+import com.ssafy.a403.domain.counselorform.repository.CounselorFormRepository;
+import com.ssafy.a403.global.config.security.LoginUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CounselorFormService {
+
+    private final CounselorFormRepository counselorFormRepository;
+
+    @Transactional
+    public void submitForm(CounselorFormRequest counselorFormRequest, LoginUser loginUser) {
+        counselorFormRepository.save(counselorFormRequest.toCounselorForm(loginUser));
+    }
+
+    @Transactional
+    public Page<CounselorForm> getAllCounselorForms(String filter, Pageable pageable) {
+
+        return counselorFormRepository.findAllPaging(filter, pageable);
+    }
 }
