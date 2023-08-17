b411675eecd2c07ebbb71a637f0e6a0a0e3eabef
soyun <soun997@gmail.com>
2023-08-17 18:11:57 +0900

    fix: router guard가 토큰 재발급 API보다 먼저 호출되는 오류 수정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainView from '../views/MainView.vue'
 import { useTokenStore } from '@/stores/token'
+import { silentReissue } from '@/api'
 
 const routes = [
   {
@@ -141,21 +142,40 @@ router.beforeEach((to) => {
   // 1. meta 확인 -> 사용자가 로그인한 상태여야 접근이 가능한 페이지라면
   // 2. tokenStore에서 사용자가 로그인했는지 여부 확인
   if (to.meta.requiresAuth && !tokenStore.isLoggedIn) {
-      alert('로그인이 필요한 페이지입니다.');
-      tokenStore.makeLoginModalVisible();
-      return '/'
+      silentReissue()
+      .catch(() => {
+        alert('로그인이 필요한 페이지입니다.');
+        tokenStore.makeLoginModalVisible();
+        return '/'
+      }
+      )
   }
   if (to.meta.onlyForUser && tokenStore.checkRolesIncludes("ROLE_COUNSELOR")) {
-      alert('이미 상담가로 등록되셨습니다.')
-      return '/'
+      silentReissue()
+      .catch(() => {
+        alert('이미 상담가로 등록되셨습니다.');
+        tokenStore.makeLoginModalVisible();
+        return '/'
+      }
+      )
   }
   if (to.meta.forCounselor && !tokenStore.checkRolesIncludes("ROLE_COUNSELOR")) {
-      alert('"상담가" 권한의 사용자만 접근가능합니다.');
-      return '/'
+      silentReissue()
+      .catch(() => {
+        alert('"상담가" 권한의 사용자만 접근가능합니다.');
+        tokenStore.makeLoginModalVisible();
+        return '/'
+      }
+      )
   }
   if (to.meta.forAdmin && !tokenStore.checkRolesIncludes("ROLE_ADMIN")) {
-      alert('"관리자" 권한의 사용자만 접근가능합니다.');
-      return '/'
+      silentReissue()
+      .catch(() => {
+        alert('"관리자" 권한의 사용자만 접근가능합니다.');
+        tokenStore.makeLoginModalVisible();
+        return '/'
+      }
+      )
   }
 })
 
